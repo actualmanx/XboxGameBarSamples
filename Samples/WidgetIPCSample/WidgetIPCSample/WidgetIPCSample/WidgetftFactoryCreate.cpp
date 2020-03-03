1ebb9fc1e2a2748b7c0d80f74227a7c6dc9ea410
Albert Burt <Albert.Burt@microsoft.com>
2020-03-03 14:58:28 -0800

    change sacl for ftserver testing
    Signed-off-by: Albert Burt <Albert.Burt@microsoft.com>

--- Samples/WidgetIPCSample/WidgetIPCSample/WidgetIPCSample/WidgetftFactoryCreate.cpp
+++ Samples/WidgetIPCSample/WidgetIPCSample/WidgetIPCSample/WidgetftFactoryCreate.cpp
@@ -9,16 +9,17 @@ namespace winrt::WidgetIPCSample::implementation
     winrt::WidgetFTServer::WidgetftFactory WidgetftFactoryCreate()
     {
         winrt::com_ptr<IActivationFactory> widgetftComFactory;
-        winrt::check_hresult(CoCreateInstance(
-            __uuidof(WidgetftComFactory),
-            nullptr,
-            CLSCTX_LOCAL_SERVER,
-            __uuidof(IActivationFactory),
-            reinterpret_cast<void**>(widgetftComFactory.put_void())));
 
+        check_hresult(CoCreateInstance(
+                __uuidof(WidgetftComFactory),
+                nullptr,
+                CLSCTX_LOCAL_SERVER,
+                __uuidof(IActivationFactory),
+                reinterpret_cast<void**>(widgetftComFactory.put_void())));
+    
         winrt::com_ptr<::IInspectable> widgetftFactoryInsp;
         widgetftComFactory->ActivateInstance(widgetftFactoryInsp.put());
-        auto widgetftFactoryObj = widgetftFactoryInsp.as<winrt::WidgetFTServer::WidgetftFactory>();
-        return widgetftFactoryObj;
+      auto widgetftFactoryObj = widgetftFactoryInsp.as<winrt::WidgetFTServer::WidgetftFactory>();
+		return widgetftFactoryObj;
     }
 }
